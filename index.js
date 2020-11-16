40c94b57649665455c48912e3ab1be3f5718e137
s-satsangi <s.satsangi@gmail.com>
2020-11-15 23:36:42 -0500

    fun stuff

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (arr,event){
+    let cards = [];
+    for(let i = 0; i<arr.length ; i++){
+        cards.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+} 
+
+function countDown(num){
+    while (num > -1){
+        console.log(num);
+        num --;
+    }
+}
